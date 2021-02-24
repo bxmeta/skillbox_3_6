146312f5f6aae653a4375075ae4e0b6b3409592f
Vadim SM <bxmeta@ya.ru>
2021-02-24 14:54:10 +0400

    ProductList component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="catalog__list">
+    <li class="catalog__item" v-for="product in products" :key="product.id">
+      <a class="catalog__pic" href="#">
+        <img :src="product.thumb.src" :alt="product.thumb.alt">
+      </a>
+
+      <h3 class="catalog__title">
+        <a href="#">
+          {{ product.name }}
+        </a>
+      </h3>
+
+      <span class="catalog__price">
+        {{ product.price }} ₽
+      </span>
+
+      <ul class="colors colors--black">
+        <li class="colors__item">
+          <label class="colors__label">
+            <input class="colors__radio sr-only"
+                   type="radio" name="color-1"
+                   value="#73B6EA" checked="">
+            <span class="colors__value" style="background-color: #73B6EA;"></span>
+          </label>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['products'],
+  name: 'ProductList',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/data/products.js
@@ -0,0 +1,85 @@
+const products = [
+  {
+    id: 1,
+    name: 'Товар 1',
+    price: 100,
+    thumb: {
+      src: 'https://picsum.photos/id/1/200/300',
+      alt: 'товар 1',
+    },
+  },
+  {
+    id: 2,
+    name: 'Товар 2',
+    price: 200,
+    thumb: {
+      src: 'https://picsum.photos/id/2/200/300',
+      alt: 'товар 2',
+    },
+  },
+  {
+    id: 3,
+    name: 'Товар 3',
+    price: 300,
+    thumb: {
+      src: 'https://picsum.photos/id/3/200/300',
+      alt: 'товар 3',
+    },
+  },
+  {
+    id: 4,
+    name: 'Товар 4',
+    price: 400,
+    thumb: {
+      src: 'https://picsum.photos/id/4/200/300',
+      alt: 'товар 4',
+    },
+  },
+  {
+    id: 5,
+    name: 'Товар 5',
+    price: 600,
+    thumb: {
+      src: 'https://picsum.photos/id/5/200/300',
+      alt: 'товар 5',
+    },
+  },
+  {
+    id: 6,
+    name: 'Товар 6',
+    price: 100,
+    thumb: {
+      src: 'https://picsum.photos/id/6/200/300',
+      alt: 'товар 6',
+    },
+  },
+  {
+    id: 7,
+    name: 'Товар 7',
+    price: 100,
+    thumb: {
+      src: 'https://picsum.photos/id/7/200/300',
+      alt: 'товар 7',
+    },
+  },
+  {
+    id: 8,
+    name: 'Товар 8',
+    price: 100,
+    thumb: {
+      src: 'https://picsum.photos/id/8/200/300',
+      alt: 'товар 8',
+    },
+  },
+  {
+    id: 9,
+    name: 'Товар 9',
+    price: 100,
+    thumb: {
+      src: 'https://picsum.photos/id/9/200/300',
+      alt: 'товар 9',
+    },
+  },
+];
+
+export default products;
